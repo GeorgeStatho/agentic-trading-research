ab16deed35ddccdad60b1076efdfdf48c32b3e68
GeorgeStatho <georgestathopou@gmail.com>
2026-03-30 03:01:34 -0400

    MOD:Made cleaner

--- Python Scripts/Webscraping/article_scraper.py
+++ Python Scripts/Webscraping/article_scraper.py
@@ -1,2 +1,8 @@
-from engine.article_scraper import ArticleSpider, crawl_articles, extract_links, extract_search_links
+from engine.article_scraper import (
+    ArticleSpider,
+    crawl_article_pages,
+    crawl_articles,
+    extract_links,
+    extract_search_links,
+)
 
