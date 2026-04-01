62d60d3bab2825532c4b5c76f5c9ade3ae396eaa
GeorgeStatho <georgestathopou@gmail.com>
2026-04-01 16:51:40 -0400

    FEAT:sector processed news to industry gets saved to database as own table now

--- Data/db_helpers/__init__.py
+++ Data/db_helpers/__init__.py
@@ -3,6 +3,7 @@ from db_helpers.market import initialize_market_database, list_companies_by_indu
 from db_helpers.news import (
     add_company_news_article,
     add_industry_news_article,
+    add_industry_opportunist_impact,
     add_sector_news_article,
     add_us_news_article,
     add_us_news_sector_impact,
@@ -13,6 +14,7 @@ from db_helpers.news import (
     list_industry_news_articles,
     load_macro_events,
     load_news_articles,
+    mark_industry_opportunist_article_processed,
     mark_us_news_article_processed,
     mark_world_news_article_processed,
 )
--- Data/db_helpers/news.py
+++ Data/db_helpers/news.py
@@ -2,6 +2,7 @@ from news_db import initialize_database as initialize_news_database
 from news_db import (
     add_company_news_article,
     add_industry_news_article,
+    add_industry_opportunist_impact,
     add_sector_news_article,
     add_us_news_article,
     add_us_news_sector_impact,
@@ -11,6 +12,7 @@ from news_db import (
     list_industry_news_articles,
     load_macro_events,
     load_news_articles,
+    mark_industry_opportunist_article_processed,
     mark_us_news_article_processed,
     mark_world_news_article_processed,
 )
