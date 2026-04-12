e151f22f04f9804e7d372618508cc20f6ba9bcf2
GeorgeStatho <georgestathopou@gmail.com>
2026-04-12 15:13:04 -0400

    Started work on frontend using typescript and react and react graphs

---
+++ web_dashboard/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Graph from './Graph.tsx'
+import OrderCardList from './orderCard.tsx'
+function App() {
+  return (
+    <>
+    <Graph />
+    <OrderCardList />
+    </>
+  )
+}
+
+export default App
